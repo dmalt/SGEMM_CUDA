734f6d6ad1f84c01c2756e6e3c63f6bf2e108f26
Dmitrii Altukhov <dmitrii.altukhov@creativefabrica.com>
2026-08-19 13:13:25 +0200

    fix

--- src/runner.cu
+++ src/runner.cu
@@ -156,7 +156,7 @@ void run_sgemm_naive(int M, int N, int K, float alpha, float *A, float *B,
   sgemm_naive<<<gridDim, blockDim>>>(M, N, K, alpha, A, B, beta, C);
 }
 
-void run_sgemm_dmalt_naive(int M, int N, int K, float alpha, float *A,
+void run_sgemm_naive_dmalt(int M, int N, int K, float alpha, float *A,
                            float *B, float beta, float *C) {
   dim3 gridDim(CEIL_DIV(M, 32), CEIL_DIV(N, 32));
   dim3 blockDim(32, 32);
