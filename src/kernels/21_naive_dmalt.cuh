37e956e0caa33a06ad95709b59184f882f8262c2
Dmitrii Altukhov <dmitrii.altukhov@creativefabrica.com>
2026-08-19 12:59:43 +0200

    fixes

--- src/kernels/21_naive_dmalt.cuh
+++ src/kernels/21_naive_dmalt.cuh
@@ -1,7 +1,7 @@
 #pragma once
 #include <cuda_runtime.h>
 
-__global__ void sgemm_naive(int M, int N, int K, float alpha, const float *A, 
+__global__ void sgemm_naive_dmalt(int M, int N, int K, float alpha, const float *A, 
     const float *B, float beta, float *C) {
   const uint x = blockIdx.x * blockDim.x + threadIdx.x;  // row number
   const uint y = blockIdx.y * blockDim.y + threadIdx.y;  // col number
