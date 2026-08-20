6b6a5245841ff716ea18b4b36b3da050f916fd50
Dmitrii Altukhov <dmitrii.altukhov@creativefabrica.com>
2026-08-20 11:31:39 +0200

    add coalescing kernels

--- src/kernels/21_naive_dmalt.cuh
+++ src/kernels/21_naive_dmalt.cuh
@@ -15,3 +15,35 @@ __global__ void sgemm_naive_dmalt(int M, int N, int K, float alpha, const float 
   }
 
 }
+
+__global__ void sgemm_coalesce_indswap_dmalt(int M, int N, int K, float alpha, const float *A, 
+    const float *B, float beta, float *C) {
+  const uint x = blockIdx.x * blockDim.x + threadIdx.x;  // row number
+  const uint y = blockIdx.y * blockDim.y + threadIdx.y;  // col number
+
+  if (y < M && x < N) {
+    float acc = 0.0;
+    for (int i = 0; i < K; i++) {
+      acc += A[y * K + i] * B[i * N + x];
+    }
+    C[y * N + x] = alpha * acc + beta * C[y * N + x];
+  }
+
+}
+
+
+template <const uint BLOCKSIZE>
+__global__ void sgemm_coalesce_dmalt(int M, int N, int K, float alpha, const float *A, 
+    const float *B, float beta, float *C) {
+  const uint iRow = blockIdx.x * BLOCKSIZE + (threadIdx.x / BLOCKSIZE);
+  const uint iCol = blockIdx.y * BLOCKSIZE + (threadIdx.x % BLOCKSIZE);
+
+  if (iRow < M && iCol < N) {
+    float acc = 0.0;
+    for (int i = 0; i < K; i++) {
+      acc += A[iRow * K + i] * B[i * N + iCol];
+    }
+    C[iRow * N + iCol] = alpha * acc + beta * C[iRow * N + iCol];
+  }
+
+}
