858bf048f6e19870585de602d02bf404e9a24469
Dmitrii Altukhov <dmitrii.altukhov@creativefabrica.com>
2026-08-26 22:36:17 +0200

    fix C indexing

--- src/kernels/21_naive_dmalt.cuh
+++ src/kernels/21_naive_dmalt.cuh
@@ -164,10 +164,9 @@ __global__ void sgemm1DBlocktilingDmalt(int M, int N, int K, float alpha,
       }
     }
 
-
     for (int iResIdx = 0; iResIdx < TM; ++iResIdx) {
-      C[(tRow * TM + iResIdx) * M + tCol] = alpha * threadResults[iResIdx]
-        + beta * C[(tRow * TM + iResIdx) * M + tCol];
+      C[(tRow * TM + iResIdx) * N + tCol] = alpha * threadResults[iResIdx]
+        + beta * C[(tRow * TM + iResIdx) * N + tCol];
     }
     __syncthreads();
   }
